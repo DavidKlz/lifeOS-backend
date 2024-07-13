70399a1be1dc1890e3728576d5b8a84a12736e35
DavidKlz <dev@david-klotz.de>
2024-07-13 09:37:55 +0200

    Init

---
+++ src/main/kotlin/de/dklotz/lifeos/LifeOsApplication.kt
@@ -0,0 +1,11 @@
+package de.dklotz.lifeos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LifeOsApplication
+
+fun main(args: Array<String>) {
+	runApplication<LifeOsApplication>(*args)
+}
---
+++ src/test/kotlin/de/dklotz/lifeos/LifeOsApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.dklotz.lifeos
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LifeOsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
